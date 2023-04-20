6cc751c21ab85b6843f69fe677b136c14a22b2da
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-04-20 21:38:28 +0300

    keeper app part1 completed

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+    return (    
+        <>
+            <Header />
+            <Note />
+            <Footer />
+        </>
+    )
+
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
+
+    return <footer><p>Copyright {year}</p></footer>
+}
+
+export default Footer;
